c9d6baf5c14eb9b3a7c35616ae854686ad9e7450
Karsten <karsten.saetre@gmail.com>
2017-06-22 23:28:42 +0200

    ADD reusable functions for parsing and added unique accessories

--- PoeOrgResourceScraper.py
+++ PoeOrgResourceScraper.py
@@ -3,45 +3,66 @@ import json
 import re
 
 urlBase = "http://pathofexile.gamepedia.com/"
-skillList = []
+skillGemsList = []
+uniqueAccessoriesList = []
 
-url='https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20skill%20gems'
-r = requests.get(url)
+urlSkillGems = 'https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20skill%20gems'
+urlUniqueAccessories = "https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20unique%20accessories"
 
-
-skills_obj = json.loads(r.text)
-#print (skills_obj['query'])
-for i in skills_obj['query']['sobj']:
-    for j in i['data']:
-        if j['property'] == '_ASKST':
-            for r in j['dataitem']:
-                # print(r['item'])
-                skillList.append(r['item'])
+skillGemsRequest = requests.get(urlSkillGems)
+uniqueAccessoriesRequest = requests.get(urlUniqueAccessories)
 
 
-def makeOrgLink (stringToBeReplaced):
+
+def getNamesFromJson(urlRequestResult):
+    tempList = []
+    jsonObj = json.loads(urlRequestResult.text)
+    # print (skills_obj['query'])
+    for i in jsonObj['query']['sobj']:
+        for j in i['data']:
+            if j['property'] == '_ASKST':
+                for name in j['dataitem']:
+                    # print(r['item'])
+                    tempList.append(name['item'])
+    return tempList
+
+def listCleaner(listToBeCleaned):
+    regex = "^\[\[\:"
+    for i, name in enumerate(listToBeCleaned):
+        # print(re.search(regex, name))
+        if re.match(regex, name) is None:
+            listToBeCleaned.remove(name)
+    return listToBeCleaned
+
+def makeOrgLinks (listToBeChanged):
     # +LINK: Spell_Echo_Support http://pathofexile.gamepedia.com/Spell_Echo_Support
-    replaced = re.sub('[\][:]', '', stringToBeReplaced)
-    replaced = re.sub('[\s]', '_', replaced)
-    skillName = replaced
-    replaced = re.sub('\'', '%27', replaced)
-    replaced = re.sub('^', urlBase, replaced)
-    replaced = re.sub('^', '#+LINK: '+skillName+' ', replaced)
-    return replaced
-
-
-
-for i,skill in enumerate(skillList):
-    skillList[i] = makeOrgLink(skill)
-
+    for i, name in enumerate(listToBeChanged):
+        replaced = re.sub('[\][:]', '', name)
+        replaced = re.sub('[\s]', '_', replaced)
+        skillName = replaced
+        replaced = re.sub('\'', '%27', replaced)
+        replaced = re.sub('^', urlBase, replaced)
+        replaced = re.sub('^', '#+LINK: '+skillName+' ', replaced)
+        listToBeChanged[i] = replaced
+    return listToBeChanged
 
 def writeResultToFile(listToWrite):
     f = open('poeOrgResources', 'w')
     for stringToWrite in listToWrite:
         f.write(stringToWrite+'\n')  # python will convert \n to os.linesep
-
     f.close()  # you can omit in most cases as the destructor will call it
 
+# Prepare active and support skill gems
+skillGemsList = getNamesFromJson(skillGemsRequest)
+skillGemsList = listCleaner(skillGemsList)
+skillGemsList = makeOrgLinks(skillGemsList)
+print(skillGemsList)
 
-del skillList[0]; # Removing the first entry as it is not a skill
-writeResultToFile(skillList)
+# Prepare Unique accessores
+uniqueAccessoriesList = getNamesFromJson(uniqueAccessoriesRequest)
+uniqueAccessoriesList = listCleaner(uniqueAccessoriesList)
+uniqueAccessoriesList = makeOrgLinks(uniqueAccessoriesList)
+print(uniqueAccessoriesList)
+
+mergedList = skillGemsList + uniqueAccessoriesList
+writeResultToFile(mergedList)
