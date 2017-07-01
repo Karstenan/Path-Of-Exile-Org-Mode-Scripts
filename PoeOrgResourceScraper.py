0f8672d3310dc5ea041b6ba6101a892832b7c9eb
Karsten A.S. Sætre <karsten.saetre@gmail.com>
2017-07-01 10:53:16 +0200

    ADD Essences to the list.

--- PoeOrgResourceScraper.py
+++ PoeOrgResourceScraper.py
@@ -14,14 +14,13 @@ uniqueJewelsList = []
 essencesList = []
 
 ############################### JSON URL LIST ##############################################
-urlSkillGems = 'https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20skill%20gems'
+urlSkillGems = "https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20skill%20gems"
 urlUniqueAccessories = "https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20unique%20accessories"
 urlUniqueArmours = "https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20unique%20armour"
 urlUniqueWeapons = "https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20unique%20weapons"
 urlUniqueFlasks = "https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20unique%20flasks"
 urlUniqueJewels = "https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20unique%20jewels"
-
-urlEssences = ""
+urlEssences = "https://pathofexile.gamepedia.com/api.php?format=json&action=browsebysubject&subject=List%20of%20essences"
 
 # Returns a list with relevant data values(names) from json 
 def getNamesFromJson(urlRequestResult):
@@ -49,6 +48,7 @@ def listCleaner(listToBeCleaned):
         listToBeCleaned[i] = replaced
 
     tempList = []
+    # Removes all entries not wanted in the final result and prints them
     for i, name in enumerate(listToBeCleaned):
         if re.match("^Has\s.*$", name):
             print (name)
@@ -82,6 +82,7 @@ uniqueArmoursRequest = requests.get(urlUniqueArmours)
 uniqueWeaponsRequest = requests.get(urlUniqueWeapons)
 uniqueFlasksRequest = requests.get(urlUniqueFlasks)
 uniqueJewelsRequest = requests.get(urlUniqueJewels)
+essencesRequest = requests.get(urlEssences)
 
 
 ################## Clean and generate lists of gems, items and jewels  ###############
@@ -91,9 +92,11 @@ uniqueArmoursList = makeNameList(uniqueArmoursList, uniqueArmoursRequest)
 uniqueWeaponsList = makeNameList(uniqueWeaponsList, uniqueWeaponsRequest)
 uniqueFlasksList = makeNameList(uniqueFlasksList, uniqueFlasksRequest)
 uniqueJewelsList = makeNameList(uniqueJewelsList, uniqueJewelsRequest)
+essencesList = makeNameList(essencesList, essencesRequest)
+
 
 ########################## Merge all lists into one and write to file ################
-mergedListOfNames = skillGemsList + uniqueAccessoriesList + uniqueArmoursList + uniqueWeaponsList + uniqueFlasksList + uniqueJewelsList
+mergedListOfNames = skillGemsList + uniqueAccessoriesList + uniqueArmoursList + uniqueWeaponsList + uniqueFlasksList + uniqueJewelsList + essencesList
 
 writeResultToFile(mergedListOfNames, 'listOfLinkNames')
 
